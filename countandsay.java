30a2672b9c4244966adf260f6cd72f992a51f15c
shafeer2511 <126599497+shafeer2511@users.noreply.github.com>
2024-05-29 15:01:25 +0530

    Create countandsay.java
    commit

---
+++ countandsay.java
@@ -0,0 +1,25 @@
+class countandsay {
+    public String countAndSay(int n) {
+        String curr="1";
+        for(int k=1;k<n;k++)
+        {
+            String next="";
+            int i=0;
+            while(i<curr.length())
+            {
+                int count=1;
+                while(i<curr.length()-1 && (curr.charAt(i)==curr.charAt(i+1)))
+                {
+                    count++;
+                    i++;
+                }
+                next+=Integer.toString(count)+curr.charAt(i);
+                i++;
+
+            }
+            curr=next;
+        }
+        return curr;
+    }
+
+}
