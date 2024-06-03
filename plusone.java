530d94b6b4367837fd11b2077e2686c33c9b33d7
shafeer2511 <126599497+shafeer2511@users.noreply.github.com>
2024-06-03 21:32:08 +0530

    Create plusone.java

---
+++ plusone.java
@@ -0,0 +1,21 @@
+public class Solution {
+    public int[] plusOne(int[] digits)
+    {
+        int n=digits.length;
+        for(int i=n-1;i>=0;i--)
+        {
+            if(digits[i]<9)
+            {
+                digits[i]++;
+                return digits;
+            }
+            //if digit is 9 then set that particular i value to 0 and move to the next index
+            digits[i]=0;
+        }
+        int newNumber[]=new int[n+1];
+        newNumber[0]=1;
+        return newNumber;
+    }
+} {
+    
+}
