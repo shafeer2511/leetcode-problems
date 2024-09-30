73e408b56db382529e2d24b77f108a68043cb5c8
Shafeer Ahamed M <126599497+shafeer2511@users.noreply.github.com>
2024-09-30 22:23:25 +0530

    Time: 0 ms (100%), Space: 41.8 MB (48.09%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -2,6 +2,42 @@ class Solution
 {
     public void sortColors(int[] nums) 
     {
-        Arrays.sort(nums);     
+        // case 0: swap with the element at the start pointer and increment start and middle
+        //case 1: no swapping only increament middle
+        //case 2: swap with the end pointer and decrement end 
+
+        //stop when the middle pointer has crossed the end pointer ... impplying that the array is sorted
+
+        int start=0;
+        int mid=0;
+        int end=nums.length-1;
+    while(mid<=end)
+    {
+
+        switch(nums[mid])  
+        {
+            case 0:
+            swap(nums,start,mid);
+            mid++;
+            start++;
+            break;
+
+            case 1:
+            mid++;
+            break;
+
+            case 2:
+            swap(nums,mid,end);
+            end--;
+            break;
+
+        }
+    }
+    }
+    public void swap(int nums[],int a,int b)
+    {
+        int temp=nums[a];
+        nums[a]=nums[b];
+        nums[b]=temp;
     }
 }
